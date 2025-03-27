dedde813ea5b37d4b84f066ba2a558125f00c43e
hontat <hontat@simplywerx.com>
2025-03-27 11:53:32 +0800

    added custom deck driver and custom app layer.

---
+++ src/deck/drivers/interface/hello.h
@@ -0,0 +1,8 @@
+#ifndef _HELLO_DECK_H_
+#define _HELLO_DECK_H_
+
+bool isFlyMode(void);
+void onErrorLed(void);
+void offErrorLed(void);
+
+#endif /* _HELLO_DECK_H_ */
--- src/deck/drivers/src/hello.c
+++ src/deck/drivers/src/hello.c
@@ -3,10 +3,28 @@
 
 #include "deck.h"
 
+#include "hello.h"
+
+bool isFlyMode(void) {
+  return digitalRead(DECK_GPIO_IO4) == LOW;
+}
+
+void onErrorLed(void) {
+  digitalWrite(DECK_GPIO_IO1, HIGH);
+}
+
+void offErrorLed(void) {
+  digitalWrite(DECK_GPIO_IO1, LOW);
+}
 
 static void helloInit()
 {
   DEBUG_PRINT("Hello Crazyflie 2.1 deck world!\n");
+  // setup error LED
+  pinMode(DECK_GPIO_IO1, OUTPUT);     // Set my error Led pin to output
+  digitalWrite(DECK_GPIO_IO1, LOW);   // Off
+  // setup switch input
+  pinMode(DECK_GPIO_IO4, INPUT_PULLUP);      //  Set my switch pin to input
 }
 
 static bool helloTest()
@@ -16,6 +34,11 @@ static bool helloTest()
 }
 
 static const DeckDriver helloDriver = {
+  .vid = 0,
+  .pid = 0,
+
+  .usedGpio = (DECK_USING_IO_1 | DECK_USING_IO_4),
+
   .name = "myHello",
   .init = helloInit,
   .test = helloTest,
